9f9d2d5366d9d6ed925d119ce96643aa0e22b729
Solange Baron <so_baron@hotmail.com>
2020-11-22 17:19:29 +0100

    Changement favicon

--- view/backend/template.php
+++ view/backend/template.php
@@ -7,7 +7,7 @@
         <meta name="description" content="Vrai Oufo">
 
         <title><?= $title ?></title>
-        <link rel="icon" sizes="192x192" href="public/img/vraioufo_favicon.png">
+        <link rel="icon" sizes="192x192" href="public/img/vreufo_favicon.png">
         
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
         <link type="text/css" rel="stylesheet" href="public/css/style.css" />
